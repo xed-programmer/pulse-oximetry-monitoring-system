d03d616598ae252ff08b1184d754050fcecd3f0b
xed-programmer <xedprogrammer@gmail.com>
2022-09-21 19:09:52 +0800

    asd

---
+++ resources/views/index.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.app')
+
+@section('header')
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+  <div class="container" data-aos="zoom-out" data-aos-delay="100">
+    <h1>Pulse Oximetry <br><span>Monitoring System</span></h1>
+    <div class="d-flex">
+      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+    </div>
+  </div>
+</section><!-- End Hero -->
+@endsection
+
+@section('content')
+<!-- ======= Featured Services Section ======= -->
+<section id="featured-services" class="featured-services">
+  <div class="container" data-aos="fade-up">
+
+    <div class="row">
+
+      <div class="col-md-6 col-lg-3 offset-md-3 d-flex align-items-stretch mb-5 mb-lg-0">
+        <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+          <div class="icon"><i class="bx bx-tachometer"></i></div>
+          <h4 class="title"><a href="">Pulse Rate Sensor</a></h4>
+          <p class="description">
+            Utilize a pulse sensor device that measures SpO2 levels.
+          </p>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+        <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
+          <div class="icon"><i class="bx bx-world"></i></div>
+          <h4 class="title"><a href="">Access Through Internet</a></h4>
+          <p class="description">
+            capable of simultaneously monitoring the SpO2 levels of patients accessible remotely without human
+            intervention
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Featured Services Section -->
+
+<!-- ======= About Section ======= -->
+<section id="about" class="about section-bg">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-title">
+      <h2>About</h2>
+      <h3>Find Out More <span>About Us</span></h3>
+      <p>Monitor oxygen saturation levels, and readily identify patients
+        in need of support.</p>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+        <img src="{{ asset('BizLand/assets/img/pulse-oximeter.jpg') }}" class="img-fluid" alt="">
+      </div>
+      <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up"
+        data-aos-delay="100">
+        <h3>BOOM Dev</h3>
+        <p>
+          Blood Oxygen Online Monitoring Device or in short
+          ‘BOOM Dev’. BOOM Dev is connected to a network that displays the status of each client
+          device as well as alerting the medical staff or concerned personnel who are monitoring
+          the status of the patient and will send an alert notification remotely on the internet if
+          SpO2 levels are detected below the threshold.
+        </p>
+      </div>
+    </div>
+
+  </div>
+</section><!-- End About Section -->
+
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-title">
+      <h2>Contact</h2>
+      <h3><span>Contact Us</span></h3>
+      <p>If you want to know something, contact us.</p>
+    </div>
+
+    <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+      <div class="col-lg-6 col-md-6">
+        <div class="info-box  mb-4">
+          <i class="bx bx-envelope"></i>
+          <h3>Email Us</h3>
+          <p>xedprogrammer@gmail.com</p>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6">
+        <div class="info-box  mb-4">
+          <i class="bx bx-phone-call"></i>
+          <h3>Call Us</h3>
+          <p>09510592362</p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+      <div class="col-lg-12">
+        <form action="#" method="post" role="form" class="php-email-form">
+          <div class="row">
+            <div class="col form-group">
+              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+            </div>
+            <div class="col form-group">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+          </div>
+          <div class="my-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Send Message</button></div>
+        </form>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Contact Section -->
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>POMS</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  {{--
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> --}}
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('BizLand/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('BizLand/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('BizLand/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('BizLand/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('BizLand/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('BizLand/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
+
+  @stack('links')
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('BizLand/assets/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a
+            href="mailto:xedprogrammer@gmail.com">xedprogrammer@gmail.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>09510592362</span></i>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="{{ route('home') }}">POMS<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="{{ route('home') }}">Home</a></li>
+          <li><a class="nav-link" href="{{ route('home') }}#about">About</a></li>
+          <li><a class="nav-link" href="{{ route('home') }}#contact">Contact</a></li>
+          @auth
+          <li>
+            <a href="#" class="nav-link">Patients</a>
+          </li>
+          @if(auth()->user())
+          <li>
+            <a href="#" class="nav-link">Admin</a>
+          </li>
+          @endif
+          <li class="dropdown"><a href="#"><span>{{ Str::limit(Auth::user()->name,15) }}</span> <i
+                class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">
+                 <p>Your Profile
+                  <br>
+                {{ Str::limit(Auth::user()->email, 22) }}</a></li>
+                 </p>
+              <li>
+                <form action="{{ route('logout') }}" method="post">
+                  @csrf
+                  <input type="submit" value="Logout" class="btn btn-light btn-block">
+                </form>
+              </li>
+            </ul>
+          </li>
+
+          @else
+          <li>
+            <a href="{{ route('login') }}" class="nav-link">Login</a>
+          </li>
+          <li>
+            <a href="{{ route('register') }}" class="nav-link">Register</a>
+          </li>
+          @endauth
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  @yield('header')
+
+  <main id="main" data-aos="fade-up">
+    @yield('content')
+  </main>
+  @yield('modals')
+
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+  {{-- <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
+
+  {{-- <script src="{{ asset('BizLand/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script> --}}
+  <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('BizLand/assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{ asset('BizLand/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{ asset('BizLand/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{ asset('BizLand/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{ asset('BizLand/assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+  <script src="{{ asset('BizLand/assets/vendor/php-email-form/validate.js')}}"></script>
+  <x-toastr-scripts />
+  @stack('scripts')
+  <!-- Template Main JS File -->
+  <script src="{{ asset('BizLand/assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
